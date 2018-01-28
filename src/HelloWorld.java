4565d14cf6e95cc9fffe3d56df9446e0dd389dd7
chakkaravarthi <chakkaravarthi_kannan@cable.comcast.com>
2018-01-28 15:14:50 -0500

    Added new line
    New SOP for my dear

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWrold {
 	
-	public static void main(String [] arg) {
-		System.out.println("Hello, World");
+	public static void main(String arg[]) {
+		System.out.println("Hello, World!");
+		System.out.println("Hello, my dear!");
 	}
 }
